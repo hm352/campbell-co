dd9f9f696b07106f1be5be0e818302d3ee13534a
Henry Maher <henry.maher99@gmail.com>
2020-03-04 14:10:31 +0000

    playing with animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import styled, { keyframes } from 'styled-components';
+import { bounce } from 'react-animations';
 
 function importAll(r) {
   return r.keys().map(r);
@@ -9,11 +11,32 @@ function importAll(r) {
 
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
-console.log(images[0])
+console.log(images)
 
 const imageParts = images.map((image) => 
     <img src={image}/>
 );
+
+//  Bootstrap, styled-component, animation example
+const bounceAnimation = keyframes`${bounce}`;
+
+const BouncyDiv = styled.button`
+  animation: 1s ${bounceAnimation};
+`;
+
+const StyledButton = styled(Button)`
+  animation: ls ${bounceAnimation};
+  background: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+
+
+//  main app render function
 function App() {
   return (
     <div className="App">
@@ -31,11 +54,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <BouncyDiv>
+      </BouncyDiv>
+      <Button infinte ></Button>
+      <StyledButton></StyledButton>
       <div>
         {imageParts}
       </div>
       <div>
-        
+
       </div>
     </div>
   );
